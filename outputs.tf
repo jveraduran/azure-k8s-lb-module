e6e9a411a08ac4785526ed517e2a5dfdc14a4ed0
j0v0057 <javier.vera@walmart.com>
2020-01-18 18:15:34 -0300

    Fix(Output LoadBalancer IP)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,19 @@
-output "load_balancer_public_ip" {
-  value = azurerm_public_ip.public_ip.*.ip_address
-}
-
-output "load_balancer_private_ip" {
-  value = azurerm_lb.load_balancer.private_ip_address
+output "load_balancer_ip" {
+  value = "${azurerm_public_ip.public_ip.ip_address}"
 }
 
 output "lb_address_pool_id" {
-  value = azurerm_lb_backend_address_pool.address_pool.id
+  value = "${azurerm_lb_backend_address_pool.address_pool.id}"
+}
+
+output "lb_id" {
+  value = "${azurerm_lb.load_balancer.id}"
+}
+
+output "lb_frontend_ip_configuration_name" {
+  value = "${azurerm_public_ip.public_ip.name}"
+}
+
+output "lb_probe_id" {
+  value = "${azurerm_lb_probe.lb_probe.id}"
 }
