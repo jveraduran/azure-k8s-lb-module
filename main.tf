648659dad753913de9524394f38243f1e72d79cd
j0v0057 <javier.vera@walmart.com>
2020-01-18 18:00:55 -0300

    Fix(ResourceGroup Variable Fix)

--- main.tf
+++ main.tf
@@ -1,63 +1,61 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
 data "azurerm_resource_group" "main" {
-  name = var.resource-group
+  name = "${var.main-resource-group}"
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  count               = var.lb_type == "public" ? 1 : 0
-  name                = "${var.cluster-name}-${var.environment}-${var.name-suffix}-pip"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-  allocation_method   = "Static"
+  name                         = "${var.cluster-name}-${var.environment}-${var.name-suffix}-pip"
+  location                     = "${data.azurerm_resource_group.main.location}"
+  resource_group_name          = "${data.azurerm_resource_group.main.name}"
+  public_ip_address_allocation = "static"
 }
 
 resource "azurerm_lb" "load_balancer" {
-  name                = "${var.cluster-name}-${var.environment}-${var.lb-type}-${var.name-suffix}-lb"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  name                = "${var.cluster-name}-${var.environment}-${var.name-suffix}-lb"
+  location            = "${data.azurerm_resource_group.main.location}"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
 
   frontend_ip_configuration {
-    name                          = "${var.cluster-name}-${var.environment}-${var.lb-type}-${var.name-suffix}-frontend"
-    public_ip_address_id          = var.lb-type == "public" ? join("", azurerm_public_ip.public_ip.*.id) : ""
-    subnet_id                     = var.subnet-id
-    private_ip_address_allocation = var.frontend-private-ip-address-allocation
-    private_ip_address            = var.frontend-private-ip-address-allocation == "Static" ? var.frontend-private-ip-address : ""
+    name                 = "${azurerm_public_ip.public_ip.name}"
+    public_ip_address_id = "${azurerm_public_ip.public_ip.id}"
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "address_pool" {
-  name                = "${var.cluster-name}-${var.environment}-${var.lb-type}-${var.name-suffix}-workers"
-  resource_group_name = data.azurerm_resource_group.main.name
-  loadbalancer_id     = azurerm_lb.load_balancer.id
-}
-
-resource "azurerm_lb_rule" "lb_rule" {
-  count                          = length(var.lb-ports)
-  resource_group_name            = data.azurerm_resource_group.main.name
-  loadbalancer_id                = azurerm_lb.load_balancer.id
-  name                           = element(keys(var.lb-ports), count.index)
-  protocol                       = values(var.lb-ports)[count.index][1]
-  frontend_port                  = values(var.lb-ports)[count.index][0]
-  backend_port                   = values(var.lb-ports)[count.index][2]
-  frontend_ip_configuration_name = "${var.cluster-name}-${var.environment}-${var.lb-type}-${var.name-suffix}-frontend"
-  enable_floating_ip             = false
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.address_pool.id
-  idle_timeout_in_minutes        = 5
-  probe_id                       = element(concat(azurerm_lb_probe.lb_probe.*.id, list("")), count.index)
-  depends_on                     = [azurerm_lb_probe.lb_probe]
+  name                = "${var.cluster-name}-${var.environment}-${var.name-suffix}-workers"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.load_balancer.id}"
 }
 
 resource "azurerm_lb_probe" "lb_probe" {
-  count               = length(var.lb-ports)
-  resource_group_name = data.azurerm_resource_group.main.name
-  loadbalancer_id     = azurerm_lb.load_balancer.id
-  name                = element(keys(var.lb-ports), count.index)
-  protocol            = values(var.lb-ports)[count.index][4] != "" ? "http" : "Tcp"
-  port                = values(var.lb-ports)[count.index][3]
-  interval_in_seconds = var.lb-probe-interval
-  number_of_probes    = var.lb_probe_unhealthy_threshold
-  request_path        = values(var.lb-ports)[count.index][4] != "" ? values(var.lb-ports)[count.index][4] : ""
+  name                = "${var.cluster-name}-${var.environment}-${var.name-suffix}-lb-probe"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.load_balancer.id}"
+  protocol            = "http"
+  request_path        = "${var.lb-probe-request-path}"
+  port                = "${var.lb-probe-port}"
+  interval_in_seconds = 15
+}
+
+resource "azurerm_lb_rule" "lb_rule_http" {
+  name                           = "${var.cluster-name}-${var.environment}-${var.name-suffix}-lb-rule-http"
+  resource_group_name            = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id                = "${azurerm_lb.load_balancer.id}"
+  protocol                       = "tcp"
+  frontend_port                  = 80
+  backend_port                   = "${var.lb-rule-port-http}"
+  frontend_ip_configuration_name = "${azurerm_public_ip.public_ip.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.address_pool.id}"
+  probe_id                       = "${azurerm_lb_probe.lb_probe.id}"
+}
+
+resource "azurerm_lb_rule" "lb_rule_https" {
+  name                           = "${var.cluster-name}-${var.environment}-${var.name-suffix}-lb-rule-https"
+  resource_group_name            = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id                = "${azurerm_lb.load_balancer.id}"
+  protocol                       = "tcp"
+  frontend_port                  = 443
+  backend_port                   = "${var.lb-rule-port-https}"
+  frontend_ip_configuration_name = "${azurerm_public_ip.public_ip.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.address_pool.id}"
+  probe_id                       = "${azurerm_lb_probe.lb_probe.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,50 +1,31 @@
-variable "main-resource-group" {
-  type = string
-}
-
-variable "lb-type" {
-  type    = string
-  default = "public"
+variable "name-suffix" {
+  type = "string"
 }
 
 variable "cluster-name" {
-  type = string
+  type = "string"
 }
 
 variable "environment" {
-  type = string
+  type = "string"
 }
 
-variable "name-suffix" {
-  type = string
+variable "main-resource-group" {
+  type = "string"
 }
 
-variable "lb-ports" {
-  type    = map(list(string))
-  default = {}
+variable "lb-probe-port" {
+  type = "string"
 }
 
-variable "lb-probe-interval" {
-  type    = string
-  default = 5
+variable "lb-probe-request-path" {
+  type = "string"
 }
 
-variable "lb-probe-unhealthy-threshold" {
-  type    = string
-  default = 2
+variable "lb-rule-port-http" {
+  type = "string"
 }
 
-variable "subnet-id" {
-  type    = string
-  default = ""
-}
-
-variable "frontend-private-ip-address-allocation" {
-  type    = string
-  default = "Dynamic"
-}
-
-variable "frontend-private-ip-address" {
-  type    = string
-  default = ""
+variable "lb-rule-port-https" {
+  type = "string"
 }
